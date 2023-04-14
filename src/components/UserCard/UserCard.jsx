79c97d718d4745ba0e2a1bdc179a627187fef809
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-04-14 13:57:38 +0000

    Update UserCard.jsx

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,14 +1,14 @@
-const UserCard = ({users}) => {
+const UserCard = ({ users }) => {
   return (
     <>
       <p>UserCard</p>
       {users.map(({ id, user, tweets, followers, avatar }) => (
         <ul key={id}>
           <li>
-              <p>{user}</p>
-              <p>{tweets}</p>
-              <p>{followers}</p>
-              <p>{avatar}</p>
+            <p>{user}</p>
+            <p>{tweets}</p>
+            <p>{followers}</p>
+            <img src={avatar} width="50" alt="avatar"></img>
           </li>
         </ul>
       ))}
