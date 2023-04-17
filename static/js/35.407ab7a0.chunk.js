1d38d6f519d4175454a8379676809948a3031fd5
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-04-18 01:38:52 +0300

    Updates

---
+++ static/js/35.407ab7a0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktweet_cards=self.webpackChunktweet_cards||[]).push([[35],{2035:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(1087),s=n(184),c=function(){return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)("p",{children:"HomePage"}),(0,s.jsx)(r.rU,{to:"/tweets",children:"tweets"})]})}}}]);
+//# sourceMappingURL=35.407ab7a0.chunk.js.map
