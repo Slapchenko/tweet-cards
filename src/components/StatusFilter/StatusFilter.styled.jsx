8b6f11e144cc2784fde783ca0ee80be7f32e11d8
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-04-18 02:01:38 +0300

    update

--- src/components/StatusFilter/StatusFilter.styled.jsx
+++ src/components/StatusFilter/StatusFilter.styled.jsx
@@ -15,7 +15,7 @@ export const FilterButton = styled.button`
   line-height: 22px;
   border: none;
   color: #373737;
-  background-color: ${({ follow }) => (follow ? "#5cd3a8" : "#ebd8ff")};
+  background-color: ${(props) => (props.selected ? "#5cd3a8" : "#ebd8ff")};
   padding: 10px 20px;
   border-radius: 10px;
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,16 +1,37 @@
-import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = 'https://643d083af0ec48ce904fe02f.mockapi.io';
+axios.defaults.baseURL = "https://643d083af0ec48ce904fe02f.mockapi.io";
 
 export const fetchUsers = createAsyncThunk(
-  'users/fetchAll',
+  "users/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('/users');
+      const url = new URL("/users", axios.defaults.baseURL);
+      url.searchParams.append("page", 1);
+      url.searchParams.append("limit", 8);
+
+      const response = await axios.get(url.toString());
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
+
+// import axios from 'axios';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// axios.defaults.baseURL = 'https://643d083af0ec48ce904fe02f.mockapi.io';
+
+// export const fetchUsers = createAsyncThunk(
+//   'users/fetchAll',
+//   async (_, thunkAPI) => {
+//     try {
+//       const response = await axios.get('/users');
+//       return response.data;
+//     } catch (e) {
+//       return thunkAPI.rejectWithValue(e.message);
+//     }
+//   }
+// );
