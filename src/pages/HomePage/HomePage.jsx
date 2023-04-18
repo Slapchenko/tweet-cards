88188ff0e77010ca620718a3668e10fd1dc96862
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-04-19 01:06:52 +0300

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom";
+import { Wrapper, Title } from "./HomePage.styled";
 
 const HomePage = () => {
   return (
-    <>
-      <p>HomePage</p>
-      <Link to="/tweets">
-        tweets
-      </Link>
-    </>
+    <Wrapper>
+      <Title>
+        Hi there! 👋 Welcome to our website! On the second page, you will find
+        user cards with all the information about them. If you want to stay
+        updated on their news and events, don't forget to subscribe to them by
+        clicking on the button below. Head over to the tweets page and enjoy
+        engaging with interesting people!
+      </Title>
+    </Wrapper>
   );
 };
 
---
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const Title = styled.p`
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: uppercase;
+  color: #5736a3;
+  margin-bottom: 26px;
+`;
