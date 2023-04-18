af78b9f015aef56a21460a6a73acd594295e92ba
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-04-19 00:35:54 +0300

    update

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchUsers, toggleFollow } from "./operations";
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://643d083af0ec48ce904fe02f.mockapi.io';
+axios.defaults.baseURL = "https://643d083af0ec48ce904fe02f.mockapi.io";
 
 const initialState = {
   items: [],
@@ -33,7 +33,9 @@ export const usersSlice = createSlice({
         state.isLoading = false;
         state.error = null;
         const updatedUser = action.payload;
-        const userIndex = state.items.findIndex((user) => user.id === updatedUser.id);
+        const userIndex = state.items.findIndex(
+          (user) => user.id === updatedUser.id
+        );
         if (userIndex >= 0) {
           state.items[userIndex] = updatedUser;
         }
@@ -46,53 +48,4 @@ export const usersSlice = createSlice({
   },
 });
 
-
 export const usersReducer = usersSlice.reducer;
-
-// import { createSlice, createAction } from "@reduxjs/toolkit";
-// import { fetchUsers } from "./operations";
-
-// const initialState = {
-//   items: [],
-//   isLoading: false,
-//   error: null,
-// };
-
-// const handlePending = (state) => {
-//   state.isLoading = true;
-// };
-
-// const handleRejected = (state, action) => {
-//   state.isLoading = false;
-//   state.error = action.payload;
-// };
-
-// export const usersSlice = createSlice({
-//   name: "users",
-//   initialState,
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(fetchUsers.fulfilled, (state, action) => {
-//         state.isLoading = false;
-//         state.error = null;
-//         state.items = action.payload;
-//       })
-//       .addCase(toggleFollow, (state, action) => {
-//         const user = state.items.find((user) => user.id === action.payload);
-//         if (user) {
-//           user.follow = !user.follow;
-//           user.followers = user.follow
-//             ? user.followers + 1
-//             : user.followers - 1;
-//         }
-//       })
-//       .addMatcher((action) => action.type.endsWith("/pending"), handlePending)
-//       .addMatcher(
-//         (action) => action.type.endsWith("/rejected"),
-//         handleRejected
-//       );
-//   },
-// });
-
-// export const toggleFollow = createAction("users/toggleFollow");
-// export const usersReducer = usersSlice.reducer;
